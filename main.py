485770b28c464d285548d2f3cd5348a59e9fa5ba
Fechter14 <trevsinz@yahoo.com>
2025-03-28 22:42:59 -0400

    add clock for FPS

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
@@ -18,6 +20,8 @@ def main():
         color = (0,0,0)
         screen.fill(color)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+        #dt = clock.get_time()/1000
 
 if __name__ == "__main__":
     main()
